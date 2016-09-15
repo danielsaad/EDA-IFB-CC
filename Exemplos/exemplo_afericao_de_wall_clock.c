50f6017b63977a721b44aac8c968c621ce7dd298
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2016-09-15 07:47:27 -0300

    Adicionando exemplo de aferição d tempo

---
+++ Exemplos/exemplo_afericao_de_wall_clock.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+void f(){
+  int i;
+  for(i=0;i<10000000;i++);
+}
+int main(){
+  struct timeval start,end;
+  gettimeofday(&start, NULL);
+  f();
+  gettimeofday(&end, NULL);
+  printf("Tempo = %ld microssegundos\n",
+    (end.tv_sec-start.tv_sec)*1000000 + (end.tv_usec-start.tv_usec));
+  return 0;
+}
