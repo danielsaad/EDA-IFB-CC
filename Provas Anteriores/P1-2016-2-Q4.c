0d5a2837fba77d9b1422c08cc352ee3a8a71c24c
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2018-09-06 14:06:13 -0300

    Adicionando ex 4 da prova anterior 2016/2

--- Provas Anteriores/P1-2016-2-Q4.c
+++ Provas Anteriores/P1-2016-2-Q4.c
@@ -3,12 +3,12 @@
 int verifica_isolada(int** matriz,int n,int cidade){
 	int k;
 	for(k=0;k<n;k++){
-		if(matriz[cidade][k]){
+		if(m[cidade][k]){
 			return 0;
 		}
 	}	
 	for(k=0;k<n;k++){
-		if(matriz[k][cidade]){
+		if(m[k][cidade]){
 			return 0;
 		}
 	}
@@ -28,7 +28,7 @@ int nro_cidades_chegam(int** matriz, int n, int cidade){
 void imprime_chega_mais(int** matriz,int n){
 	int cidade;
 	int maximo = 0;
-	int* contador = calloc(sizeof(int),n);
+	int* contador = calloc(n,sizeof(int));
 	for(cidade=0;cidade<n;cidade++){
 	 	contador[cidade] = nro_cidades_chegam(matriz,n,cidade);
 		if(contador[cidade]>maximo){
@@ -48,7 +48,7 @@ void imprime_isoladas(int** matriz,int n){
 	int cidade;
 	for(cidade=0;cidade<n;cidade++){
 		if(verifica_isolada(matriz,n,cidade)){
-			printf("A cidade %d é isolado.\n");
+			printf("A cidade %d é isolado.\n",cidade);
 		}
 	}
 }
